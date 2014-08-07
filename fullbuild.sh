d4c10bb112cae9685f002f963ae6e3b66a034842
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-08-07 18:02:06 +0530

    added gpl v3

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,4 +1,24 @@
 
+
+# BUILD SCRIPT
+
+ #Build script to apply all patches for galaxy grand duos
+
+ #Copyright (c) 2014 Rutvik Rajagopal
+
+ #This program is free software: you can redistribute it and/or modify
+ #it under the terms of the GNU General Public License as published by
+ #the Free Software Foundation, either version 3 of the License, or
+ #(at your option) any later version.
+
+ #This program is distributed in the hope that it will be useful,
+ #but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ #GNU General Public License for more details.
+
+ #You should have received a copy of the GNU General Public License
+ #along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #! /bin/sh
 #this script copies the patches from the patch folder to the rom folder 
 # just replace the ~/patch to ur folder name where the patches exist and change ~/cm11 to the path of the rom 
