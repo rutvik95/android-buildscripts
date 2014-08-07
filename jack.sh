a61a46a567617c40b00e4a0343342097a31a72db
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-08-07 18:03:30 +0530

    added gpl v3

--- jack.sh
+++ jack.sh
@@ -1,3 +1,22 @@
+# BUILD SCRIPT
+
+ #Build script to compile jackeagle xda's maintained roms for galaxy grand duos
+
+ #Copyright (c) 2014 Rutvik Rajagopal
+
+ #This program is free software: you can redistribute it and/or modify
+ #it under the terms of the GNU General Public License as published by
+ #the Free Software Foundation, either version 3 of the License, or
+ #(at your option) any later version.
+
+ #This program is distributed in the hope that it will be useful,
+ #but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ #GNU General Public License for more details.
+
+ #You should have received a copy of the GNU General Public License
+ #along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 ﻿#!/bin/bash
 #This build script has to be kept in the home folder along with the patch folder .. it will automatically navigate and apply patches and build
 
